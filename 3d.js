91a11d3e0b3c30446d728e6ad80802ffd65e592c
substack <substack@bits.coop>
2020-01-17 00:22:19 -1000

    3d

--- 3d.js
+++ 3d.js
@@ -1,24 +1,24 @@
-var dot = require('gl-vec2/dot')
-var cross = require('gl-vec2/cross')
-var length = require('gl-vec2/length')
+var dot = require('gl-vec3/dot')
+var cross = require('gl-vec3/cross')
+var length = require('gl-vec3/length')
 
 var EPSILON = 0.00001
-var tmpv0 = [0,0]
-var tmpv1 = [0,0]
+var tmpv0 = [0,0,0]
+var tmpv1 = [0,0,0]
 
-module.exports = function sat2d (A, B, epsilon) {
+module.exports = function sat3d (A, B, epsilon) {
   if (epsilon === undefined) epsilon = EPSILON
   for (var i = 0; i < A.separatingAxes.length; i++) {
-    if (!check(A, B, A.separatingAxes[i])) return false
+    if (!check(A,B,A.separatingAxes[i])) return false
   }
   for (var i = 0; i < B.separatingAxes.length; i++) {
-    if (!check(A, B, B.separatingAxes[i])) return false
+    if (!check(A,B,B.separatingAxes[i])) return false
   }
   for (var i = 0; i < A.separatingAxes.length; i++) {
     for (var j = 0; j < B.separatingAxes.length; j++) {
       cross(tmpv0,A.separatingAxes[i],B.separatingAxes[j])
       if (length(tmpv0) <= epsilon) continue
-      if (!check(A, B, tmpv0)) return false
+      if (!check(A,B,tmpv0)) return false
     }
   }
   return true
